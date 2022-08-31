fa136dc22d2b86ff687aeecf990cc73c69eb7afd
Saulo Michielin <saulomichielin@gmail.com>
2022-08-31 19:03:06 -0300

    Feito armazenamento de cores

--- script.js
+++ script.js
@@ -9,13 +9,24 @@ function alteraCores () {
         const cores = document.getElementById('cor'+index)
         cores.style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b +')';
     }
-    if ('cor1' === 'cor2' || 'cor1' === 'cor3' || 'cor2' === 'cor3') {
+    if ('cor2' === 'cor3' || 'cor2' === 'cor4' || 'cor3' === 'cor4') {
         for (let index = 2; index <= 4; index +=1) {
             let r = parseInt(Math.random()*255)
             let g = parseInt(Math.random()*255)
-            let b = parseInt(Math.random()*254) //é 254 para eliminar a possibilidade de dar branco
+            let b = parseInt(Math.random()*254)
             const cores = document.getElementById('cor'+index)
             cores.style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b +')';
         }
     }
+    let corInicial = [cor2.style.backgroundColor, cor3.style.backgroundColor, cor4.style.backgroundColor];
+    
+    localStorage.setItem('colorPalette',JSON.stringify(corInicial));
+}
+
+window.onload = function() {
+let coresIniciais = JSON.parse(localStorage.getItem('colorPalette'));
+document.getElementById('cor2').style.backgroundColor = coresIniciais[0];
+document.getElementById('cor3').style.backgroundColor = coresIniciais[1];
+document.getElementById('cor4').style.backgroundColor = coresIniciais[2];
+console.log(coresIniciais[2])
 }
